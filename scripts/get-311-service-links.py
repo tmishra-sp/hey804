dca6d9b2224a2392700b5acfe400180f716f2b33
Ben Bricker <bbricker@seerai.space>
2026-03-27 16:24:36 -0400

    add docs

--- scripts/get-311-service-links.py
+++ scripts/get-311-service-links.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python3
 import requests
+import json
 
 service_group_addr = "https://webapi.citizenservices.org/rvaone/api/v1/servicegroups"
-example_service_group_services = "https://webapi.citizenservices.org/rvaone/api/v1/servicegroups/31cd796e-e824-41c0-b770-18b0ed86c93a/services"
+
+
+"""
+Gets a list of service groups and their associated services. 
+Supplements data with source links to rva311.com for each group and service.
+Services with `category: 0` allow service request submission, `category: 1` only provides links or contact information.
+
+Example usage from repo root: `python3 ./scripts/get-311-service-links.py >> ./server/data/data-311.json`
+"""
 
 
 def get_311_data():
+    # Get all top level service group categories
     service_groups = requests.get(service_group_addr).json()
     for service_group in service_groups:
         services_addr = service_group_addr + "/" + service_group["id"] + "/services"
+        # Add source link for service group, links to 311 showing category expanded where user can select specific services
         service_group["source"] = (
             "https://www.rva311.com/rvaone#/request/new?gid=" + service_group["id"]
         )
+        # Get all services for the service group category
         services = requests.get(services_addr).json()
+        # Add source link for each service, links to creating a new service request in 311
         for service in services:
             service["source"] = (
                 "https://www.rva311.com/rvaone#/request/new/"
@@ -25,4 +38,5 @@ def get_311_data():
 
 
 if __name__ == "__main__":
-    print(get_311_data())
+    data = get_311_data()
+    print(json.dumps(data, indent=2))
