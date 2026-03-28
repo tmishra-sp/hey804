5c5dca55867858e287fb6c54504cc70e53ce1fcb
Ben Bricker <bbricker@seerai.space>
2026-03-27 22:52:38 -0400

    source api key from config and parse responses

--- server/services/language.py
+++ server/services/language.py
@@ -1,19 +1,21 @@
 import requests
-import os
+from server.config import GOOGLE_API_KEY
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "")
 GOOGLE_TRANSLATE_ROOT_URL = "https://translation.googleapis.com/language/translate/v2/"
 GOOGLE_TRANSLATE_DETECT_URL = GOOGLE_TRANSLATE_ROOT_URL + "detect"
 
 
 def detect_language(text: str) -> str:
+    if not GOOGLE_API_KEY:
+        return "en"
     try:
         res = requests.post(
             GOOGLE_TRANSLATE_DETECT_URL,
             data={"q": text, "key": GOOGLE_API_KEY},
         ).json()
         if "data" in res:
-            return res["data"][0]
+            lang = res["data"]["detections"][0][0]["language"]
+            return lang
         else:
             return "en"
     except Exception as e:
@@ -29,7 +31,8 @@ def translate_text(text: str, src_lang: str, target_lang: str) -> str:
             data={"q": text, "source": src_lang, "target": target_lang, "key": GOOGLE_API_KEY},
         ).json()
         if "data" in res:
-            return res["data"][0]
+            translated_text = res["data"]["translations"][0]["translatedText"]
+            return translated_text
         else:
             return text
     except Exception as e:
